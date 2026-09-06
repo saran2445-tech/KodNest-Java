692e52f18e3e09ddf27024f796bab02979b44fbc
saran2445-tech <saaisaran1212@gmail.com>
2026-09-07 01:02:44 +0530

    Added learning commitment program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PrintLearningCommitment.java
@@ -0,0 +1,9 @@
+public class PrintLearningCommitment {
+    public static void main(String[] args) {
+        // Display the student's learning track.
+        System.out.println("Track: Java Development");
+
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
-       System.out.println("Welcome to JAva ");
+       System.out.println("Welcome to Java ");
        System.out.println("Im in Java Track");
     }
 }
