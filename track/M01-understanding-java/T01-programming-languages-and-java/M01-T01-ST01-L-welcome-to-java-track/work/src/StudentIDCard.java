824373c901ccf755ee69c8a7e462299746aeba52
saran2445-tech <saaisaran1212@gmail.com>
2026-09-07 01:12:07 +0530

    Added student ID card program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/StudentIDCard.java
@@ -0,0 +1,11 @@
+public class StudentIDCard {
+    public static void main(String[] args) {
+        // Profile Details
+        System.out.println("----------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("----------------");
+    }
+}
