420db052d14c29fcc722b0554b21413ef852ad7f
saran2445-tech <saaisaran1212@gmail.com>
2026-09-07 01:07:49 +0530

    Added placement goal program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PlacementGoal.java
@@ -0,0 +1,10 @@
+public class PlacementGoal {
+    public static void main(String[] args) {
+        // Display the student's placement goal.
+        System.out.println("---MY PLACEMENT GOAL---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
+
