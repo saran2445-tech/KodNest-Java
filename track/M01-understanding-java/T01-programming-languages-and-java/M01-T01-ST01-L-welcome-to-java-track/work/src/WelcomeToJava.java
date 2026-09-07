cd28e7bd374627cb22b96b632641e970f60b955e
saran2445-tech <saaisaran1212@gmail.com>
2026-09-07 14:56:46 +0530

    java program

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,6 +2,5 @@ public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
        System.out.println("Welcome to Java ");
-       System.out.println("Im in Java Track");
     }
 }
